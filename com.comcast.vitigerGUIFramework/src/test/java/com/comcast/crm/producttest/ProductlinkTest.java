eb2cd7beb451e41ebc8c64f3830d5fc89600f490
Siribhavana <siribhavanaudandarao@gmail.com>
2025-12-26 13:34:31 +0530

    modified Script

--- com.comcast.vitigerGUIFramework/src/test/java/com/comcast/crm/producttest/ProductlinkTest.java
+++ com.comcast.vitigerGUIFramework/src/test/java/com/comcast/crm/producttest/ProductlinkTest.java
@@ -17,15 +17,28 @@ public class ProductlinkTest
 	public void createNewProduct() 
 	{
 		WebDriver driver=new ChromeDriver();
+		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
 		driver.get("http://49.249.28.218:8888/");
+		
 		driver.manage().window().maximize();
+		
 		driver.findElement(By.name("user_name")).sendKeys("admin");
+		
 		driver.findElement(By.name("user_password")).sendKeys("admin");
+		
 		driver.findElement(By.id("submitButton")).click();
+		
 		driver.findElement(By.linkText("Products")).click();
+		
 		driver.findElement(By.xpath("//img[@alt='Create Product...']")).click();
+		
 		driver.findElement(By.name("productname")).sendKeys("sam123");
+		
+		System.out.println("test created");
+		
 		driver.findElement(By.name("button")).click();
+		
 	}
 }
